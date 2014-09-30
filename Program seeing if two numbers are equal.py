de545b00fdef548c69d10c965ded62ebbc743cd1
Harry Robinson <41194@longroad.ac.uk>
2014-09-30 15:25:07 +0100

    programme seeing if two numbers are equal

---
+++ Program seeing if two numbers are equal.py
@@ -0,0 +1,12 @@
+#Harry Robinson
+#29-09-2014
+#Programme stating if two numbers are the same
+
+firstNumber = int(input("Give the first number: "))
+secondNumber = int(input("Give second number: "))
+if firstNumber == SecondNumber:
+    print("The numbers are equal")
+if firstNumber < secondNumber:
+    print("The numbers aren't equal")
+if firstNumber > secondNumber:
+    print("The numbers aren't equal")
