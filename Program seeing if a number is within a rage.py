b305cdea509c1bb874fbc01b0cde27e5e68f52f9
Harry Robinson <41194@longroad.ac.uk>
2014-10-03 10:22:14 +0100

    Various number manipulation programes

---
+++ Program seeing if a number is within a rage.py
@@ -0,0 +1,12 @@
+#Harry Robinson
+#02-10-2014
+#program asking for a number that is within a range
+
+print("This programme will see if an entered number is within a range")
+enteredNumber = int(input("Give a number: "))
+if enteredNumber >= 21 and enteredNumber <=29:
+    print("Number within range")
+else :
+    print("Number not in range")
+
+    
