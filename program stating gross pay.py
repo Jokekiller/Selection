67c48fc8b2dd1be90f44867c8cb9d70ed8a94476
Harry <Harry@192.168.0.6>
2014-10-14 11:08:41 +0100

    hours of work programme

--- program stating gross pay.py
+++ program stating gross pay.py
@@ -4,3 +4,16 @@
 
 hourlyRate = float(input("Give you're hourly rate: "))
 hoursWorked = float(input("Give the hours you have worked this week: "))
+if hoursWorked >= 0 and hoursWorked <= 60:
+    print("Enter a value in the range of 0-60.")
+    if hoursWorked < 40:
+        grossPay = hourlyRate * hoursWorked
+        print("grossPay = {0}".format(grossPay))
+    elif hoursWorked >= 40:
+        extraHours = hoursWorked - 40
+        extraPay = hourlyRate * 1.5
+        grossPay = (hourlyRate * 40) + (extraHours * extraPay)
+        print("grossPay = {0}".format(grossPay))
+if hoursWorked <0 or hoursWorked > 60 :
+    print("Give a vlue in the range 0-60")
+    
