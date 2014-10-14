42d977026bf43fc9bfaec6ca804bce0aab7c763d
Harry <Harry@192.168.0.6>
2014-10-14 11:08:28 +0100

    Hours of work program

---
+++ program stating gross pay.py
@@ -0,0 +1,6 @@
+#Harry Robinson
+#08-10-2014
+#Program stating the gross pay and extra hours
+
+hourlyRate = float(input("Give you're hourly rate: "))
+hoursWorked = float(input("Give the hours you have worked this week: "))
